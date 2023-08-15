9c451eaa3199f593a9f2d454f66633405cb1fb68
Nhyira <nhyiratnkansah@gmail.com>
2023-08-15 10:49:32 -0400

    to do list

--- ToDoList_Help/ContentView.swift
+++ ToDoList_Help/ContentView.swift
@@ -14,8 +14,11 @@ struct ContentView: View {
     //In Iteration 2, add @Environment here
     
     //Add a State property called toDoItems that holds an empty array of ToDoItems
+    @State var toDoItems:
+    [ToDoItem] = []
     
     //Add a State property called showNewTask that is set to false
+    @State private var showNewTask = false
     
     var body: some View {
         VStack {
@@ -24,14 +27,12 @@ struct ContentView: View {
                     .fontWeight(.bold)
                     .font(.system(size: 40))
                 
-                //Add a Text View containing the string "ToDoList" and modifiers here
-            
-                //Add a Spacer here
-                
                 Spacer()
+
                 
                 //Add a Button here with Text("+") and delete any code in the action
                 Button(action: {
+                    self.showNewTask = true
                                     
                 }) {
                 Text("+")
@@ -41,12 +42,26 @@ struct ContentView: View {
             //Add a padding modifier here
             .padding()
             //Add a Spacer here
+            Spacer()
             
+            List {
+                ForEach (toDoItems) {
+                    toDoItem in
+                    if toDoItem.isImportant == true {
+                        Text("‼️" + toDoItem.title)
+                    } else {
+                        Text(toDoItem.title)
+                    }
+                }
+            }
             //Add a List View here
                 //In Iteration 1, delte the Text View and add an if statement inside the list view that indicates to add !! to ToDoItems that are marked important
                     //In Iteration 2, add optionals to the Text views in the if statement
                         //In Iteration 2, add the onDelete modifier before the closing bracket of the For Each statement
         }
+            if showNewTask {
+                NewToDoView(title: "", isImportant: false, toDoItems: $toDoItems, showNewTask: $showNewTask)
+                }
         
         //Add an if statement here that indicates if showNewTask is true, then NewToDoView will be displayed
             //In Iteration 1, bind toDoItems: $toDoItems in this if statement
--- ToDoList_Help/NewToDoView.swift
+++ ToDoList_Help/NewToDoView.swift
@@ -23,33 +23,32 @@ struct NewToDoView: View {
         //Delete the ToDoItems array binding in iteration 2
     
     //Bind the showNewTask property here
+    @Binding var toDoItems:
+        [ToDoItem]
+    @Binding var showNewTask : Bool
     
     var body: some View {
         VStack {
-            //Add Text View containing "Task title" here
             Text("Task Title")
                 .font(.system(size: 30))
                 .fontWeight(.bold)
                 .foregroundColor(Color(hue: 1.0, saturation: 0.886, brightness: 0.573))
             
-            
-            //Add TextField here with the Text "Enter task description" and binded to the title state property (text: $title)
             TextField("Enter task desription...", text: $title)
                 .frame(width: 350.0, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
                 .border(Color.gray)
                 .padding()
                
             
-            //Add Toggle here that is binded to isImportant (isOn: $isImportant) and the text "Is it important?"
-            
             Toggle(isOn: $isImportant) {
                 Text("Is it important?")
             }
             .padding()
-            //Add a padding modifier
             
             //Add Button here, delete any code in the action and with the text "Add"
             Button(action: {
+                self.showNewTask = false
+                self.addTask(title: self.title, isImportant: self.isImportant)
               
             }) {
                 Text("Add")
@@ -67,14 +66,18 @@ struct NewToDoView: View {
             .cornerRadius(15)
         }
     }
-    //Add the private function addTask here
-        //In Iteration 2, update the function to save input to Core Data
+    private func addTask(title: String, isImportant: Bool = false) {
+            
+            let task = ToDoItem(title: title, isImportant: isImportant)
+            toDoItems.append(task)
+        }
 }
 
 struct NewToDoView_Previews:
     PreviewProvider {
     static var previews: some View {
-        NewToDoView(title: "", isImportant: false)
+        NewToDoView(title: "", isImportant: false,toDoItems: .constant([]), showNewTask: .constant(true))
+        
             //Add toDoItems: .constant([]) here (Iteration 1)
             //Add showNewTask: .constant(true) (Iteration 1)
             //Delete toDoItems: .constant([]) (Iteration 2_
